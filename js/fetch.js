58c81b924c4d694b7a8798ea6c6c4b1ed6f82304
SaidTomas <saidtomasur@gmail.com>
2022-08-12 01:16:28 -0300

    El mejor Fetch de la historia

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,19 @@
 
 
 const useFetch = () => {
-    const url = 'https://api.mercadolibre.com/sites/MLA/search?q=ropa'
+    const url = 'https://api.mercadolibre.com/sites/MLA/search?q=ropa%20de%20abrigo'
     const titulo = document.getElementById ('tituloremeras');
     fetch(url).then(res => res.json())
         .then(data => {
             var productos = data.results
-            titulo.innerHTML = productos[0].title
+            var cards = document.getElementsByClassName ("card")
+            for (let i = 0; i < cards.length; i++) {
+                var childrenCard = cards [i].children
+                childrenCard [0].src = productos[i].thumbnail
+                var childrenCardBody =  childrenCard [1].children
+                childrenCardBody [0].innerHTML = productos [i].title
+                childrenCardBody [1].innerHTML = productos [i].price
+            }
+            //titulo.innerHTML = productos[0].title
         });
 }
